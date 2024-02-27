f3344e46b9c42084a7d26065b736c7796019946f
Maria CHEREL SOROKINA <mcherel-@paul-f3Br1s1.clusters.42paris.fr>
2024-02-27 16:44:06 +0100

    nothing interestion

--- spider.py
+++ spider.py
@@ -3,7 +3,7 @@ from urllib.parse import urlparse
 import requests
 
 '''url = "https://www.example.com/path/to/resource?query=value#fragment"
-
+sys.arg -> voir en py
 # Parsing de l'URL
 parsed_url = urlparse(url)
 
@@ -19,7 +19,7 @@ ERR_FIRST_ARG = 'ERROR: First argument should be "./spider"'
 ERR_COMMAND_L = 'ERROR: The command should have at list two elements'
 command=""
 url=""
-success=0
+success=0 #global
 flag=("-r", "-l", "-p")
 
 def command_validation(boolean, error_msg):
@@ -31,11 +31,31 @@ def command_validation(boolean, error_msg):
         success=0
     return success
 
+def path_validation(command, path="./data/"):
+    global success
+    success=0
+    if "-p" in command:   
+        for i in command:
+            if i == "-p":
+                success=1
+                continue
+            if success==1:
+                path=i
+                
+                break
+    return success,path
+
+
+    
 while(not success):
     command = input(COMMAND).split()
     print(command)
     if not command_validation(len(command)>=2, ERR_COMMAND_L): continue
     if not command_validation(command[0]=="./spider", ERR_FIRST_ARG): continue
+    if not path_validation(command)[0]: 
+        continue
+    path =  path_validation(command)[1]   
+    print("path : " + path)
     print("Success : "+str(success))
     url = command[-1]
 
